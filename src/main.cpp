60be68155efd08b01e07f185605f79091b4dc3fc
matrefeytontias <mattias@refeyton.fr>
2014-06-22 12:00:53 +0200

    Updated spriteset with new sprites

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
 		printf("Couldn't allocate %d bytes for enemiesArray.\n", sizeof(Enemy) * MAX_ENEMY);
 		exit(0);
 	}
-	
+
 	for(int i = 0; i < MAX_ENEMY; i++)
 	{
 		enemiesArray[i] = new Enemy;

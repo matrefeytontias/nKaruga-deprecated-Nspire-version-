dceaf621adb15b34a170a27732b4aae29c8b1dc6
matrefeytontias <mattias@refeyton.fr>
2014-08-10 21:05:28 +0200

    Removed useless code

--- src/utils.cpp
+++ src/utils.cpp
@@ -67,9 +67,5 @@ KeyEvent getk(void)
 	k |= (((((isKeyPressed(KEY_NSPIRE_ESC) << 1) + isKeyPressed(G_fragmentKey)) << 1) + isKeyPressed(G_polarityKey)) << 1) + isKeyPressed(G_fireKey);
 	k <<= 4;
 	k |= (((((isKeyPressed(KEY_NSPIRE_UP) << 1) + isKeyPressed(KEY_NSPIRE_RIGHT)) << 1) + isKeyPressed(KEY_NSPIRE_LEFT)) << 1) + isKeyPressed(KEY_NSPIRE_DOWN);
-	/*if(isKeyPressed(KEY_NSPIRE_UPRIGHT)) k |= 0x0c;
-	if(isKeyPressed(KEY_NSPIRE_RIGHTDOWN)) k |= 0x05;
-	if(isKeyPressed(KEY_NSPIRE_DOWNLEFT)) k |= 0x03;
-	if(isKeyPressed(KEY_NSPIRE_LEFTUP)) k |= 0x0a;*/
 	return k;
 }

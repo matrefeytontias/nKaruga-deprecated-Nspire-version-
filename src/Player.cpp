185f801ca6a8053605cdcc073a0def2b2b16f894
matrefeytontias <mattias@refeyton.fr>
2015-02-11 13:28:44 +0100

    Improve firing of power fragments

--- src/Player.cpp
+++ src/Player.cpp
@@ -99,7 +99,7 @@ void Player::handle(KeyEvent kEv, BulletArray *bArray)
 			{
 				G_hasFiredOnce = true;
 				for(int i = 0; i < G_power / 10; i++)
-					bArray->add_fragment(x, y, ((i % 6) - 3) * 8 + ((i % 2) ? 128 : 0), this, polarity, false);
+					bArray->add_fragment(x, y, ((i % 2) ? -33 + (i / 2) * 11 : 161 - (i / 2) * 11), this, polarity, false);
 				G_power = 0;
 			}
 		}
